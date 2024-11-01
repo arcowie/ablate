193f5b81091bf59dba5ba688ec89089963b5ddac
Albert Cowie <arcowie@buffalo.edu>
2024-11-01 09:03:52 -0400

    Removed Option checking for GPU

--- src/utilities/petscUtilities.cpp
+++ src/utilities/petscUtilities.cpp
@@ -2,7 +2,9 @@
 #include "environment/runEnvironment.hpp"
 
 void ablate::utilities::PetscUtilities::Initialize(const char help[]) {
+
     PetscInitialize(ablate::environment::RunEnvironment::GetArgCount(), ablate::environment::RunEnvironment::GetArgs(), nullptr, help) >> utilities::PetscUtilities::checkError;
+
 
     // register the cleanup
     ablate::environment::RunEnvironment::RegisterCleanUpFunction("ablate::utilities::PetscUtilities::Initialize", []() { PetscFinalize() >> utilities::PetscUtilities::checkError; });
